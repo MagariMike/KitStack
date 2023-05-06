8d8c41a7458ed1a4e922385e4ea614f84bb7529f
Rob B <rob@rjrobbie.dev>
2023-05-06 14:26:41 +0100

    add navbar profile, logout, create item and home buttons - styled and tested

---
+++ src/__tests__/LogoutButton.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import LogoutButton from '../components/LogoutButton';
+
+describe('LogoutButton', () => {
+  it('renders logout button with right-from-bracket icon', () => {
+    render(<LogoutButton />);
+    const buttonElement = screen.getByRole('button');
+    const iconElement = screen.getByTestId('fa-right-from-bracket-icon');
+
+    expect(buttonElement).toBeInTheDocument();
+    expect(iconElement).toBeInTheDocument();
+    expect(buttonElement).toContainElement(iconElement);
+  });
+});
---
+++ src/__tests__/ProfileButton.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ProfileButton from "../components/ProfileButton";
+
+describe('ProfileButton', () => {
+    it("renders profile button with user icon", () => {
+        render(<ProfileButton />);
+        const buttonElement = screen.getByRole('button');
+        const iconElement = screen.getByTestId('fa-user-icon');
+
+        expect(buttonElement).toBeInTheDocument();
+        expect(iconElement).toBeInTheDocument();
+        expect(buttonElement).toContainElement(iconElement);
+    });
+});
