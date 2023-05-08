6d2f841d808c6c23ddfbf9589413687702c67911
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-05-08 16:20:41 +0100

    initial react router setup and refactoring to display buttons which link to correct pages

--- src/components/buttons/HomeButton.js
+++ src/components/buttons/HomeButton.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse } from '@fortawesome/free-solid-svg-icons'
-import "../styles/navbar-buttons.css"
+
 
 
 const HomeButton = () => {
--- src/components/buttons/ProfileButton.js
+++ src/components/buttons/ProfileButton.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
-import "../styles/navbar-buttons.css"
+
 
 
 const ProfileButton = () => {
