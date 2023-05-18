d59be3c226c47689001c425804252fe535468d90
josephredmond5 <josephredmond5@icloud.com>
2023-05-18 20:58:08 +0100

    changes to login.js, signup.js and log out pages

--- src/components/pages/LogoutPage.js
+++ src/components/pages/LogoutPage.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import React from "react";
+import LogoutButton from "./logoutbutton";
 
 const LogoutPage = () => {
-  return <h1>Logout Page</h1>;
+  return (
+    <div>
+      <h1>Logout Page</h1>
+      <LogoutButton /> 
+    </div>
+  );
 };
 
 export default LogoutPage;
---
+++ src/components/pages/login.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase/config';
+
+const Signin = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    
+    await signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        // ..
+    });
+};
+
+return (
+    <div>
+      <form>
+      <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} 
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} 
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter your password"
+         />
+        </div>
+        <div>
+        <button type="submit" onClick={onSubmit}>log in</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+
+
+
+export default Signin;
